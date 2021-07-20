3d3c8abc3aeaddc8b4ebfd1f42779898da529868
Michail Zyusko <michail.zyusko@gmail.com>
2021-07-20 23:17:01 +0300

    Create project

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'no-console': 'off',
+  },
+  "parser": "babel-eslint",
+};
